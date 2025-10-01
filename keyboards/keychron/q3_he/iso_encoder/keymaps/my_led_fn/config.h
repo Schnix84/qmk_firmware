d3d0a636a2a3c525fb80ee55e5d553ab2a7d5139
Matthias Schneider <mc.schneider84@gmail.com>
2025-10-01 23:56:44 +0200

    chore: add ALL_WHITE as custom effect and remove it from default

--- keyboards/keychron/q3_he/iso_encoder/keymaps/my_led_fn/config.h
+++ keyboards/keychron/q3_he/iso_encoder/keymaps/my_led_fn/config.h
@@ -1,8 +1,6 @@
-#pragma once
-
-// Starte immer in "Solid Color" und Weiß (Sättigung 0 = Weiß)
-#define RGB_MATRIX_DEFAULT_MODE RGB_MATRIX_SOLID_COLOR
-#define RGB_MATRIX_DEFAULT_HUE  0
-#define RGB_MATRIX_DEFAULT_SAT  0
-#define RGB_MATRIX_DEFAULT_VAL  255
-#define RGB_MATRIX_DEFAULT_SPD  0
+// // Set ALL_WHITE as default effect
+// #define RGB_MATRIX_DEFAULT_MODE RGB_MATRIX_CUSTOM_ALL_WHITE
+// #define RGB_MATRIX_DEFAULT_HUE  0
+// #define RGB_MATRIX_DEFAULT_SAT  0
+// #define RGB_MATRIX_DEFAULT_VAL  255
+// #define RGB_MATRIX_DEFAULT_SPD  0
